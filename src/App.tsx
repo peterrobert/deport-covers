8ca4d2899b27cb035d0fd71fd6daeca55da1ecd1
peterrobert <pwambui93@gmail.com>
2026-06-22 09:53:52 +0300

    scroll to the top component

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import About from "./pages/About";
 import Process from "./pages/Process";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
+import ScrollToTop from "./components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -19,6 +20,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/services" element={<Services />} />
